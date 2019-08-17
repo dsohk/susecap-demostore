cdd6fdd59446ed97e517dc36ae6efa7bbbac6317
Derek So <derek.so@suse.com>
2019-08-18 06:47:41 +0800

    updated dashboard to include business statistics

--- dashboard/README.md
+++ dashboard/README.md
@@ -1,1 +1,3 @@
 Check out http://smashing.github.io/smashing for more information.
+Also https://github.com/tywhang/smashing-chartjs
+
--- dashboard/jobs/suse-order.rb
+++ dashboard/jobs/suse-order.rb
@@ -18,4 +18,66 @@ SCHEDULER.every '2s', allow_overlapping: false do
   send_event('visitors',  current: visitors )
   send_event('revenue',  current: revenue )
   send_event('achieved',  value: achieved )
+
+
+  puts "Query top customer"
+  response = HTTParty.get("http://suse-order-processor.open-cloud.net/api/getTop/type/cust", headers: { 
+    "Accept" => "application/json" 
+  })
+  puts "result: #{response}" # ["Derek So","30","Roger Brown","13","Roger B","4"]
+  mylabels = []
+  mydata = []
+  response.each_slice(2) do |pair| 
+    mylabels << pair[0]
+    mydata << pair[1]
+  end
+
+  send_event("bar-chart-customer", {
+    type: "bar",
+    header: "Top 3 Customers",
+    labels: mylabels,
+    colorNames: ["green", "yellow", "blue"],
+    datasets: mydata
+  })  
+
+  puts "Query top payment type"
+  response = HTTParty.get("http://suse-order-processor.open-cloud.net/api/getTop/type/pay", headers: { 
+    "Accept" => "application/json" 
+  })
+  puts "result: #{response}"
+  mylabels = []
+  mydata = []
+  response.each_slice(2) do |pair| 
+    mylabels << pair[0]
+    mydata << pair[1]
+  end
+
+  send_event("pie-chart-pay", {
+    type: "pie",
+    header: "Payment Methods",
+    labels: mylabels,
+    colorNames: ["green", "yellow", "blue"],
+    datasets: mydata
+  }) 
+
+  puts "Query top product"
+  response = HTTParty.get("http://suse-order-processor.open-cloud.net/api/getTop/type/item", headers: { 
+    "Accept" => "application/json" 
+  })
+  puts "result: #{response}"
+  mylabels = []
+  mydata = []
+  response.each_slice(2) do |pair| 
+    mylabels << pair[0]
+    mydata << pair[1]
+  end
+
+  send_event("bar-chart-product", {
+    type: "bar",
+    header: "Top 3 Product",
+    labels: mylabels,
+    colorNames: ["green", "yellow", "blue"],
+    datasets: mydata
+  })  
+
 end
