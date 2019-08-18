e5af2a22bf9bb99a1e3d4e25f3c8c70879ceb1d4
Derek So <derek.so@suse.com>
2019-08-19 06:01:08 +0800

    fix reset

--- order-processor/server.js
+++ order-processor/server.js
@@ -23,7 +23,7 @@ app.options('*', cors(corsOptions));
 //app.use(cors(corsOptions));
 
 app.listen(process.env.VCAP_APP_PORT ||8001, function () {
-    console.log ("Server started!");    
+    console.log ("Server started!");
 });
 
 
@@ -36,18 +36,18 @@ app.post('/api/order', cors(), (req, res, next) => {
     client.hincrby("orders", "total", req.body.price);
 
     // increment products
-    client.hsetnx("products:"+req.body.product, "name",req.body.product);                                        
+    client.hsetnx("products:"+req.body.product, "name",req.body.product);
     client.hincrby("products:"+req.body.product,"total",req.body.price);
     client.hincrby("products:"+req.body.product,"count",1);
     client.sadd("all_products", req.body.product);
-    
+
     // increment pay methods
-    client.hsetnx("pays:"+req.body.paymethod, "name",req.body.paymethod);                                        
+    client.hsetnx("pays:"+req.body.paymethod, "name",req.body.paymethod);
     client.hincrby("pays:"+req.body.paymethod,"total",req.body.price);
     client.hincrby("pays:"+req.body.paymethod,"count",1);
     client.sadd("all_pays", req.body.paymethod);
-    
-    
+
+
     //increment customer
     client.zincrby("customers",1, req.body.customer);
     res.status('201').send(req.body);
@@ -99,48 +99,48 @@ function getItemByNo(req, res){
 
     client.smembers("all_products", (err, reply) => {
 
-        
+
         async.eachSeries(reply, (item, callback) => {
 
             client.hget("products:"+item, "count", (err, reply) => {
-                    
+
                 client.zadd("top_products_no", reply, item);
-                callback();                        
+                callback();
             });
         },
         (err) => {
-            client.zrevrangebyscore ("top_products_no", '+inf', '-inf', 'withscores', 
+            client.zrevrangebyscore ("top_products_no", '+inf', '-inf', 'withscores',
                     (err, reply) => {
                         res.json(reply);
                 });
-                
+
             }
         );
-        
+
     });
 }
 
 function getItemBySales(req, res){
 
-    client.smembers("all_products", (err, reply) => {      
+    client.smembers("all_products", (err, reply) => {
 
-        
+
         async.eachSeries(reply, (item, callback) => {
 
                 client.hget("products:"+item, "total", (err, reply) => {
-                        
+
                     client.zadd("top_products_sales", reply, item);
-                    callback();                        
+                    callback();
                 });
             },
             (err) => {
-                client.zrevrangebyscore ("top_products_sales", '+inf', '-inf', 'withscores', 
+                client.zrevrangebyscore ("top_products_sales", '+inf', '-inf', 'withscores',
                     (err, reply) => {
                         res.json(reply);
                 });
             }
         );
-        
+
     });
 }
 
@@ -148,48 +148,48 @@ function getPayByNo(req, res){
 
     client.smembers("all_pays", (err, reply) => {
 
-        
+
         async.eachSeries(reply, (item, callback) => {
 
             client.hget("pays:"+item, "count", (err, reply) => {
-                    
+
                 client.zadd("top_pays_no", reply, item);
-                callback();                        
+                callback();
             });
         },
         (err) => {
-            client.zrevrangebyscore ("top_pays_no", '+inf', '-inf', 'withscores', 
+            client.zrevrangebyscore ("top_pays_no", '+inf', '-inf', 'withscores',
                     (err, reply) => {
                         res.json(reply);
                 });
-                
+
             }
         );
-        
+
     });
 }
 
 function getPayBySales(req, res){
 
-    client.smembers("all_pays", (err, reply) => {      
+    client.smembers("all_pays", (err, reply) => {
 
-        
+
         async.eachSeries(reply, (item, callback) => {
 
                 client.hget("pays:"+item, "total", (err, reply) => {
-                        
+
                     client.zadd("top_pays_sales", reply, item);
-                    callback();                        
+                    callback();
                 });
             },
             (err) => {
-                client.zrevrangebyscore ("top_pays_sales", '+inf', '-inf', 'withscores', 
+                client.zrevrangebyscore ("top_pays_sales", '+inf', '-inf', 'withscores',
                     (err, reply) => {
                         res.json(reply);
                 });
             }
         );
-        
+
     });
 }
 
@@ -212,6 +212,8 @@ var reset = function () {
     client.HMSET("products:Geeko", {"count":0, "total": 0});
     client.HMSET("products:Cup", {"count":0, "total": 0});
     client.del("all_products");
+    client.del("products*");
+    client.del("pays*");
 
     client.HMSET ("pays:Cash", {"count":0, "total": 0});
     client.HMSET("pays:Credit Card", {"count":0, "total": 0});
@@ -219,6 +221,6 @@ var reset = function () {
     client.del("all_pays");
 
 
-    client.del("customers");    
+    client.del("customers");
 }
 
