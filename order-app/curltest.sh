62981d98425fa078c3b8e9899200c129bf1d230c
Derek So <derek.so@suse.com>
2019-11-07 09:02:34 +0800

    add continuous curl test script to validate zero-downtime deployment

---
+++ order-app/curltest.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# To test continuously the availability of order-app site
+# during zero-downtime deployment.
+
+while true; do
+  curl --head http://suse-order-app.open-cloud.net
+  sleep 1
+done
